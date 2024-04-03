f3678d347e81978007ae8789a7479b46c1cd64d3
shifinmalik <shifinmalik@gmail.com>
2024-04-03 11:53:52 -0700

    Up

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
 import Category3 from "./components/Category/category3.jsx";
+
 import Services from "./components/Services/Services";
 import Banner from "./components/Banner/Banner";
 import Partners from "./components/Partners/Partners.jsx";
